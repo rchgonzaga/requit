a763f016c5db638d6ca26e0ac42610db4bb14091
rchgonzaga <rchgonzaga@gmail.com>
2018-12-27 00:25:00 -0200

    Testing some forms using reactstrap with bootstrap 4

--- src/features/home/DefaultPage.js
+++ src/features/home/DefaultPage.js
@@ -7,6 +7,12 @@ import reactLogo from '../../images/react-logo.svg';
 import rekitLogo from '../../images/rekit-logo.svg';
 import * as actions from './redux/actions';
 
+import {
+  Container, Col, Form,
+  FormGroup, Label, Input,
+  Button,
+} from 'reactstrap';
+
 export class DefaultPage extends Component {
   static propTypes = {
     home: PropTypes.object.isRequired,
@@ -21,6 +27,36 @@ export class DefaultPage extends Component {
           <img src={rekitLogo} className="rekit-logo" alt="logo" />
           <h1 className="app-title">This will be a landing page in the future</h1>
         </header>
+
+      <Container className="App">
+        <h2>Sign In</h2>
+        <Form className="form">
+          <Col>
+            <FormGroup>
+              <Label>Email</Label>
+              <Input
+                type="email"
+                name="email"
+                id="exampleEmail"
+                placeholder="myemail@email.com"
+              />
+            </FormGroup>
+          </Col>
+          <Col>
+            <FormGroup>
+              <Label for="examplePassword">Password</Label>
+              <Input
+                type="password"
+                name="password"
+                id="examplePassword"
+                placeholder="********"
+              />
+            </FormGroup>
+          </Col>
+          <Button>Submit</Button>
+        </Form>
+      </Container>
+      
       </div>
     );
   }
